a164a48fac1335fdb654ad844e8030ac2c9b8d72
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-02-28 12:54:10 +0100

    ADD catch in getPostTitle

--- snacks.js
+++ snacks.js
@@ -4,14 +4,16 @@
 
 function getPostTitle(id) {
     return new Promise((resolve, reject) => {
-        fetch(`https://dummyjson.com/posts/${id}`)
+        fetch(`https://dummyjasdfson.com/posts/${id}`)
             .then((response) => response.json())
             .then((data) => resolve(data.title))
             .catch((error) => reject(error));
     });
 }
 
-// getPostTitle(1).then((title) => console.log(title));
+getPostTitle(1)
+    .then((title) => console.log(title))
+    .catch((error) => console.log(error));
 
 // Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
 
