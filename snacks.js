9b0daf52f7e3947cac126da33bcf3cd82ae317f7
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-02-28 12:50:35 +0100

    ADD snacks

---
+++ snacks.js
@@ -0,0 +1,34 @@
+// Ottieni il titolo di un post con una Promise.
+
+// Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+
+function getPostTitle(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then((response) => response.json())
+            .then((data) => resolve(data.title))
+            .catch((error) => reject(error));
+    });
+}
+
+// getPostTitle(1).then((title) => console.log(title));
+
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 6) + 1;
+            if (Math.random() < 0.2) {
+                // 20% chance
+                reject('Dado incastrato');
+            } else {
+                resolve(randomNumber);
+            }
+        }, 3000);
+    });
+}
+
+// lanciaDado()
+//     .then((number) => console.log(number))
+//     .catch((error) => console.log(error));
